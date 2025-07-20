c66ba20f677453d2766eab83b2f9e568cf18e8c5
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-07-20 09:41:57 +0200

    Improved dataset_cifar

--- src/dataset_cifar.py
+++ src/dataset_cifar.py
@@ -2,19 +2,23 @@
 danielsinkin97@gmail.com
 """
 
-from typing import TypeAlias
+from typing import Iterator, TypeAlias
 
+import matplotlib.pyplot as plt
+import torchvision
+import torchvision.utils
 from jaxtyping import Float, Int
 from torch import Tensor
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
 from .common import assert_shape
 
 batch_size, channel, height, width = 64, 3, 32, 32
-CIFARImages: TypeAlias = Float[Tensor, f"B {channel} {height} {width}"]
-CIFARLabels: TypeAlias = Int[Tensor, f"{batch_size}"]
+B_ = "B"  # Workaround for Ruff
+CIFARImages: TypeAlias = Float[Tensor, f"{B_} {channel} {height} {width}"]
+CIFARLabels: TypeAlias = Int[Tensor, f"{B_}"]
 CIFARBatch: TypeAlias = tuple[CIFARImages, CIFARLabels]
 
 
@@ -24,7 +28,19 @@ def validate_batch(batch: CIFARBatch) -> None:
     assert_shape(labels, (batch_size,))
 
 
-def get_data(train: bool = True) -> DataLoader[CIFARBatch]:
+class ImageOnlyDataset(Dataset):
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __getitem__(self, idx):
+        img, _ = self.dataset[idx]
+        return img  # drop label
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+def get_data(train: bool = True) -> DataLoader[CIFARImages]:
     transform = transforms.Compose(
         [
             transforms.Resize((height, width)),
@@ -32,8 +48,43 @@ def get_data(train: bool = True) -> DataLoader[CIFARBatch]:
             transforms.Normalize([0.5], [0.5]),
         ]
     )
-    trainset = CIFAR10(root="./data", train=train, download=True, transform=transform)
-    train_loader: DataLoader[CIFARBatch] = DataLoader(
-        trainset, batch_size=64, shuffle=True, num_workers=2, pin_memory=True
+    raw_dataset = CIFAR10(
+        root="./data", train=train, download=True, transform=transform
     )
-    return train_loader
+    image_dataset = ImageOnlyDataset(raw_dataset)
+
+    return DataLoader(
+        image_dataset, batch_size=64, shuffle=True, num_workers=2, pin_memory=True
+    )
+
+
+def get_iterator_batch(data_loader: DataLoader[CIFARBatch]) -> Iterator[CIFARBatch]:
+    return iter(data_loader)
+
+
+def get_iterator_images(data_loader: DataLoader[CIFARBatch]) -> Iterator[CIFARImages]:
+    for images, _ in data_loader:
+        yield images
+
+
+def get_iterator_labels(data_loader: DataLoader[CIFARBatch]) -> Iterator[CIFARLabels]:
+    for _, labels in data_loader:
+        yield labels
+
+
+def visualize_images_grid(images: CIFARImages, title: str = "Image Grid") -> None:
+    """
+    Visualizes a batch of images in an 8x8 grid.
+    Assumes images are normalized to [-1, 1] and denormalizes to [0, 1] for display.
+    """
+    images = images * 0.5 + 0.5
+    images = images.clamp(0.0, 1.0)
+    images = images[:64]
+    grid = torchvision.utils.make_grid(images, nrow=8)
+    np_grid = grid.permute(1, 2, 0).cpu().numpy()
+
+    plt.figure(figsize=(8, 8))  # type: ignore
+    plt.imshow(np_grid)  # type: ignore
+    plt.axis("off")  # type: ignore
+    plt.title(title)  # type: ignore
+    plt.show()  # type: ignore
