87273cd708a5d6b0a105821343f9566731b03a92
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-07-20 09:42:23 +0200

    Implemented very basic forward for DiffusionModel, it has successfully been trained

--- src/diffusion.py
+++ src/diffusion.py
@@ -3,20 +3,20 @@ danielsinkin97@gmail.com
 """
 
 import torch
-from jaxtyping import Float
+from jaxtyping import Float, Int
 from torch import Tensor, nn
 
 from .common import assert_shape
-from .dataset_cifar import CIFARImages
+from .dataset_cifar import CIFARImages, validate_batch
 
 T_ = "T"
+B_ = "B"
 
 
 def get_beta_schedule_linear(
-    T: int, beta_0: float = 1e-4, beta_T: float = 1e-1
-) -> Float[Tensor, f"{T_}"]:  # noqa: F821
-    beta_t = torch.linspace(beta_0, beta_T, T)
-    return torch.cumprod(1 - beta_t, dim=0)
+    T: int, beta_0: float = 1e-4, beta_T: float = 2e-2
+) -> Float[Tensor, f"{T_}"]:
+    return torch.linspace(beta_0, beta_T, T)
 
 
 class SinusoidalTimeEmbedding(nn.Module):
@@ -26,7 +26,7 @@ class SinusoidalTimeEmbedding(nn.Module):
 
     def forward(self, t: Tensor) -> Tensor:
         assert len(t.shape) == 1
-        batch = t.shape
+        batch = t.shape[0]
         half_dim = self.dim // 2
         freqs = torch.exp(
             -torch.arange(half_dim, dtype=torch.float32, device=t.device)
@@ -40,22 +40,41 @@ class SinusoidalTimeEmbedding(nn.Module):
 
 
 class DiffusionModel(nn.Module):
-    def __init__(self, T: int, beta_t: Float[Tensor, f"{T_}"]):
+    def __init__(self, T: int, beta_t: Float[Tensor, f"{T_}"], embed_dim: int = 64):
         super().__init__()  # type: ignore
+        self.embed_dim = embed_dim
+
         assert_shape(beta_t, (T,))
         self.T = T
-        self.alpha_bars = torch.cumprod(1 - beta_t, dim=0)
+        self.register_buffer("alpha_bars", torch.cumprod(1 - beta_t, dim=0))
 
-        self.epsilon_theta = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 3, 3, padding=1),
+        self.time_embedding = SinusoidalTimeEmbedding(dim=self.embed_dim)
+        self.time_mlp = nn.Sequential(
+            nn.Linear(self.embed_dim, self.embed_dim), nn.ReLU()
         )
 
-    def forward(self, x_t: Tensor, t: Tensor) -> Tensor:
-        return self.epsilon_theta(x_t)
+        self.conv1 = nn.Conv2d(3, self.embed_dim, 3, padding=1)
+        self.conv2 = nn.Conv2d(self.embed_dim, 3, 3, padding=1)
+
+    def forward(self, x_t: CIFARImages, t: Int[Tensor, f"{B_}"]) -> Tensor:
+        batch, channel, height, width = x_t.shape
+        t_emb = self.time_embedding(t)
+        assert_shape(t_emb, (batch, self.embed_dim))
+        batch, dim = t_emb.shape
+        t_emb = self.time_mlp(t_emb)
+        assert_shape(t_emb, (batch, dim))
+        t_emb = t_emb[:, :, None, None]
+        assert_shape(t_emb, (batch, dim, 1, 1))
+
+        h = nn.functional.relu(self.conv1(x_t) + t_emb)
+        out = self.conv2(h)
+        return out
 
     def train_step(self, x_0: Tensor) -> Tensor:
+        if x_0.dim() == 3:
+            x_0 = x_0.unsqueeze()
+            assert x_0.dim() == 4
+
         B = x_0.shape[0]
         device = x_0.device
 
